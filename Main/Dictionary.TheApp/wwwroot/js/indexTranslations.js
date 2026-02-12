62d20caeee982b2023aeb39f374f6e79af2c3588
Petr Machovec <pmachovec@outlook.com>
2026-02-12 12:25:45 +0100

    Styling enhancements

--- Main/Dictionary.TheApp/wwwroot/js/indexTranslations.js
+++ Main/Dictionary.TheApp/wwwroot/js/indexTranslations.js
@@ -3,12 +3,23 @@
 // * One-time initialization, ideal for one-time tasks like this.
 (() => {
     // Maps of translations
-    const preparationTranslations = {
-        en: 'Preparation',
-        cs: 'Příprava'
+    const loadingTranslations = {
+        en: 'Loading',
+        cs: 'Načítání'
     };
 
+    const reloadTranslations = {
+        en: 'Reload',
+        cs: 'Obnovit'
+    }
+
+    const unhandledErrorTranslations = {
+        en: 'An unhandled error has occurred',
+        cs: 'Vyskytla se neočekávaná chyba'
+    }
+
     // Pick the best language.
+    // Yes, it's based on one specific translations map. Put up with it!
     function pickLang() {
         // const langs = navigator.languages || [navigator.language || 'en'];
         const langs = ['cs']; // For now, just run in Czech.
@@ -16,13 +27,13 @@
         for (const l of langs) {
             const lc = l.toLowerCase();
 
-            if (preparationTranslations[lc]) {
+            if (loadingTranslations[lc]) {
                 return lc;
             }
 
             const base = lc.split('-')[0];
 
-            if (preparationTranslations[base]) {
+            if (loadingTranslations[base]) {
                 return base;
             }
         }
@@ -39,5 +50,7 @@
     }
 
     const lang = pickLang();
-    setTextContent(lang, 'preparation', preparationTranslations);
+    setTextContent(lang, 'loading', loadingTranslations);
+    setTextContent(lang, 'reload', reloadTranslations);
+    setTextContent(lang, 'unhandled-error', unhandledErrorTranslations);
 })();
